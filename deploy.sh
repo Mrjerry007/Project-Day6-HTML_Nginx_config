46a86977f37a412a4a2cb9c961fd6da0a5a3872e
Mrjerry007 <chiragsingh9912@gmail.com>
2025-07-06 03:11:15 +0530

    New detials provided

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,14 @@
+## Upadating the system and installing necessary libaries
+sudo yun update -y
+sudo yum install nginx -y
+#  installing nano text editor for writing Index.html and style.css files
+sudo yum install nano -y
+
+
+# run this code to create path for fiel
+sudo nano /usr/share/nginx/html/index.html
+# And use Index.html and style.css or write it
+
+
+# Restart the Nginx
 sudo systemctl restart nginx
